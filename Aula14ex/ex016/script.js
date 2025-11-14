871b1124b28289a01459d6cf442b576b5b20da83
Gustavo Gadelha <Guztv04@gmail.com>
2025-11-14 14:23:34 -0300

    modulo E finalizado

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,21 +1,34 @@
 function contar(){
-    let divResultado = document.getElementById("res")
-    let numInic = document.getElementById("txtInic")
-    let numFim = document.getElementById("txtFim")
-    let numPasso = document.getElementById("txtPasso")
+    // caixas que serão preenchidas 
+    let inic = document.getElementById('txtInic')
+    let fim = document.getElementById('txtFim')
+    let passo = document.getElementById('txtPasso')
+    //  div para o resultado
+    let resultado = document.getElementById('res')
 
 
 
-    if (numInic.Value.length == 0 || numFim.Value.length == 0 || numPasso.Value.length == 0){
+    if (inic.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert("[ERRO] Faltam dados!")
     }else{
-        divResultado.innerHTML = "contando: "
-        let i = Number(numInic.value)
-        let f = Number(numFim.value)
-        let p = NUmber(numPasso.value)
+        resultado.innerHTML = "contando: "
+        let i = Number(inic.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
 
-        for(let x = i; x < f; x += p){
-            divResultado.innerHTML += `${x}`
+        if (i < f){
+            for(let cont = i; cont <= f; cont += p){
+                resultado.innerHTML += `${cont} \u{1F449}`
+            }
+        }else{
+            for(cont = i; cont >= f; cont -= p){
+                resultado.innerHTML += `${cont} \u{1F449} `
+            }
         }
+
+        resultado.innerHTML += `\u{1F3C1}`
     }
+
+    
+
 }
