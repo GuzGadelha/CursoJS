5b3fccbcb27ada9105814b0f402ce8af1c865f64
Gustavo Gadelha <Guztv04@gmail.com>
2026-02-15 16:32:09 -0300

    final do curso

--- Aula16/ex018/script.js
+++ Aula16/ex018/script.js
@@ -1,8 +1,43 @@
-function push(){
-    let num = Number(document.getElementById("txtNum").value);
+    let num = document.querySelector('input#txtNum');
+    let lista = document.querySelector('select#selTab');
+    let res = document.querySelector('div#res');
+    let array = [];
 
-    let array = [num];
+    function isNumber(n) {
+        if (Number(n) >= 1 && Number(n) <= 100) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    function inLista(n, l) {
+        if (l.indexOf(Number(n)) != -1) {
+            return true; 
+        }else{
+            return false;
+        }
+    }
 
-    
+    function adicionar(){
+        if (isNumber(num.value) && !inLista(num.value, array)) {
 
-}
+            array.push(Number(num.value));
+            let item = document.createElement('option');
+            item.text = `Número ${num.value} adicionado`;
+            lista.appendChild(item);
+            
+            
+        }else{
+            window.alert("Valor inválido ou já inserido na lista!")
+        }
+    }
+
+    function finalizar(){
+        /*
+        - quant total de elemetnos
+        - maior elemetno
+        - menor elemento
+        - somatorio
+        - media
+        */
+    }
